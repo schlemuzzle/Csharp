3339320d6f483858e6d8a0f649053838c2619797
schlemuzzle <schlemuzzle@mail.ru>
2023-06-09 14:06:12 +0300

    Add Ex_005ConditionIfElse

---
+++ Less_01/Ex_005ConditionIfElse/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите ваше имя: ");
+string username = Console.ReadLine();
+
+if (username.ToLower() == "маша")
+{
+    Console.WriteLine("Ура, это же МАША!!!");
+}
+else 
+{
+    Console.Write("Ну здорово, ");
+    Console.WriteLine(username);
+}
