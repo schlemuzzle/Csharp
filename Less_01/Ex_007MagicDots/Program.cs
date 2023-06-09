367f57d9e26d214e0eb7bb171033be00599ddfcc
schlemuzzle <schlemuzzle@mail.ru>
2023-06-09 15:23:06 +0300

    Add Ex_007MagicDots

--- Less_01/Ex_007MagicDots/Program.cs
+++ Less_01/Ex_007MagicDots/Program.cs
@@ -1,2 +1,42 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Clear();
+// Console.SetCursorPosition(10, 4);
+// Console.WriteLine("+");
+
+int xa = 30, ya = 1,
+    xb = 1, yb = 20,
+    xc = 60, yc = 20;
+
+Console.SetCursorPosition(xa, ya);
+Console.WriteLine("+");
+
+Console.SetCursorPosition(xb, yb);
+Console.WriteLine("+");
+
+Console.SetCursorPosition(xc, yc);
+Console.WriteLine("+");
+
+int x = xa, y = yb;
+int count = 0;
+
+while (count < 1000)
+{   
+    int eks = new Random().Next(0, 3); // [0, 3) 0, 1, 2
+    if(eks == 0)
+    {
+        x = (x + xa)/2;
+        y = (y + ya)/2;
+    }
+    if(eks == 1)
+    {
+        x = (x + xb)/2;
+        y = (y + yb)/2;
+    }
+    if(eks == 2)
+    {
+        x = (x + xc)/2;
+        y = (y + yc)/2;
+    }
+    Console.SetCursorPosition(x, y);
+    Console.WriteLine("+");
+    count++;
+}
