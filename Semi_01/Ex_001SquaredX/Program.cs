34b6965001fe389faf9bf3240706a95573b18756
schlemuzzle <schlemuzzle@mail.ru>
2023-06-10 14:39:34 +0300

    Add Ex_001SquaredX

---
+++ Semi_01/Ex_001SquaredX/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.Clear();
+int num = int.Parse(Console.ReadLine()); // Преобразует данные с консоли из 'string' в 'int'
+// Первый способ - математический:
+int num2 = num * num;
+Console.WriteLine($"Первый способ num^2 -> {num2}");
+// Второй способ - при помощи библиотеки:
+int num3 = Convert.ToInt32(Math.Pow(num, 2)); // 32 - размер ячейки памяти для числа (можно и 16) 
+// Math - наименование бибилиотеки, Pow - функция возведения в степень (на выходе будет 'double', поэтому конвертируем в 'int')
+Console.WriteLine($"Второй способ num^2 -> {num3}");
