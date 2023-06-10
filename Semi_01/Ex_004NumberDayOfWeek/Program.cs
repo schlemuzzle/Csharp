900227eb75545cf1ae14626e8d826470f330a33e
schlemuzzle <schlemuzzle@mail.ru>
2023-06-10 16:32:41 +0300

    Add Ex_004NumberDayOfWeek

---
+++ Semi_01/Ex_004NumberDayOfWeek/Program.cs
@@ -0,0 +1,37 @@
+﻿Console.Clear();
+Console.Write("Введите номер дня недели: ");
+int numberDayOfWeek = int.Parse(Console.ReadLine());
+if (numberDayOfWeek < 1 || numberDayOfWeek > 7) // || - или (можно использовать && - и)
+{
+    Console.WriteLine("Введен неправильный номер");
+    return; // эта команда завершает выполнение программы
+}
+
+if (numberDayOfWeek == 1)
+{
+    Console.WriteLine("Понедельник");
+}
+else if (numberDayOfWeek == 2)
+{
+    Console.WriteLine("Вторник");
+}
+else if (numberDayOfWeek == 3)
+{
+    Console.WriteLine("Среда");
+}
+else if (numberDayOfWeek == 4)
+{
+    Console.WriteLine("Четверг");
+}
+else if (numberDayOfWeek == 5)
+{
+    Console.WriteLine("Пятница");
+}
+else if (numberDayOfWeek == 6)
+{
+    Console.WriteLine("Суббота");
+}
+else if (numberDayOfWeek == 7)
+{
+    Console.WriteLine("Воскресенье");
+}
