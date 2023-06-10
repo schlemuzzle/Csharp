26a33b3b2bd14a99d9a2ce80fde88d2962ef0916
schlemuzzle <schlemuzzle@mail.ru>
2023-06-10 17:54:16 +0300

    Add Ex_01+gitignore

---
+++ DZ_Semi_01/Ex_01MinMax2/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Clear();
+Console.WriteLine("Введине первое число: ");
+int a = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите второе число: ");
+int b = int.Parse(Console.ReadLine());
+if (a > b)
+{
+    Console.WriteLine($"max={a}");
+}
+else
+{
+    Console.WriteLine($"max={b}");
+}
