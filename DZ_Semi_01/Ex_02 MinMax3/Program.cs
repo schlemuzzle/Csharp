37dcba342b3f246222d870d4bca484f064b90a13
schlemuzzle <schlemuzzle@mail.ru>
2023-06-10 18:07:24 +0300

    Add Ex_02

---
+++ DZ_Semi_01/Ex_02 MinMax3/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.Clear();
+Console.WriteLine("Введите первое число: ");
+int a = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите второе число: ");
+int b = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите третье число: ");
+int c = int.Parse(Console.ReadLine());
+if (a > b && a > c)
+{
+    Console.WriteLine($"max={a}");
+}
+else if (a < b && b > c)
+{
+    Console.WriteLine($"max={b}");
+}
+else
+{
+    Console.WriteLine($"max={c}");
+}
