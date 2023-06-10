248fa6ab7999e8338cec9423806e6f0fc1797a0c
schlemuzzle <schlemuzzle@mail.ru>
2023-06-10 17:16:00 +0300

    Add Ex_006NRangeAbs

---
+++ Semi_01/Ex_006NRangeAbs/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.Clear();
+Console.Write("Введите число n: ");
+int n = int.Parse(Console.ReadLine());
+int b = (-1) * Math.Abs(n); // Math.Abs - модуль числа
+while (b <= Math.Abs(n))
+{
+    Console.Write($"{b} "); // $ "{b}" позволяет использовать переменную b в строке
+    b++;
+}
