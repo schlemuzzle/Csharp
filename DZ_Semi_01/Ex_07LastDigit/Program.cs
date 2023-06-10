7c65f536a58e8cc14fcb39fabefd92bbfd9ba25c
schlemuzzle <schlemuzzle@mail.ru>
2023-06-10 18:46:26 +0300

    Add Ex_07

---
+++ DZ_Semi_01/Ex_07LastDigit/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Clear();
+Console.Write("Введите трехзначное число: ");
+int a = int.Parse(Console.ReadLine());
+if (a < 99 || a > 999)
+{
+    Console.WriteLine("Введено не трехзначное число");
+    return; 
+}
+int b = a % 10;
+Console.WriteLine($"Последняя цифра: {b}");
