e309b40b1216a2f1f83f6f2f68ff6842512aca7d
schlemuzzle <schlemuzzle@mail.ru>
2023-06-10 18:14:14 +0300

    Add Ex_03

---
+++ DZ_Semi_01/Ex_03Even/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Clear();
+Console.WriteLine("Введите число: ");
+int a = int.Parse(Console.ReadLine());
+if (a%2 == 0)
+{
+    Console.WriteLine($"Да, {a} - четное");
+}
+else
+{
+    Console.WriteLine($"Нет, {a} - не четное");
+}
