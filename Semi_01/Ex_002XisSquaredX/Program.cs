a3d3837d7abe40cb7f3012f5a8aa3f3d3eb1ed67
schlemuzzle <schlemuzzle@mail.ru>
2023-06-10 15:16:11 +0300

    Add Ex_002XisSquaredX

---
+++ Semi_01/Ex_002XisSquaredX/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Clear();
+Console.Write("Введите первое число: ");
+int num1 = int.Parse(Console.ReadLine()); 
+Console.Write("Введите второе число: ");
+int num2 = int.Parse(Console.ReadLine()); 
+if(num1 == num2 * num2)
+{
+    Console.WriteLine("Да, первое является квадратом второго");
+}
+else
+{
+    Console.WriteLine("Нет, первое не является квадратом второго");
+}
