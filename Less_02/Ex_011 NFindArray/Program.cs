483910646cf6057efca869216c1ea3217e50d2f2
schlemuzzle <schlemuzzle@mail.ru>
2023-06-13 14:16:02 +0300

    Add Ex_011NFindArray

---
+++ Less_02/Ex_011 NFindArray/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.Clear();
+int [] array = {13, 23, 56, 22, 78, 98, 55, 44};
+
+int n = array.Length;
+int find = 44;
+int index = 0;
+
+while (index < n)
+{
+    if (array[index] == find)
+    {
+        Console.WriteLine(index);
+        break; // прерывает цикл,на случай, если несколько одинаковых искомых значений элементов в массиве
+    }
+    index++;
+}
