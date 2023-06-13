3f8c826a69e6d3c76e41f6e4d632d4f35866bb60
schlemuzzle <schlemuzzle@mail.ru>
2023-06-13 14:01:14 +0300

    Add Ex_010MaxArray

---
+++ Less_02/Ex_010MaxArray/Program.cs
@@ -0,0 +1,15 @@
+﻿int Max(int arg1, int arg2, int arg3)
+{
+    int result = arg1;
+    if (arg2 > result) result = arg2;
+    if (arg3 > result) result = arg3;
+    return result;
+}
+//индексы:       0   1   2   3   4   5   6   7   8
+int [] array = {13, 23, 56, 22, 78, 98, 55, 44, 29};
+
+int max = Max(Max(array[0], array[1], array[2]), 
+              Max(array[3], array[4], array[5]), 
+              Max(array[6], array[7], array[8]));
+
+Console.WriteLine(max);
