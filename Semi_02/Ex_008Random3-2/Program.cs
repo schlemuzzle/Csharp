0076078dbd8d6384970dce9a65972e951af7d916
schlemuzzle <schlemuzzle@mail.ru>
2023-06-14 15:21:15 +0300

    Add Ex_008Random3-2

---
+++ Semi_02/Ex_008Random3-2/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.Clear();
+int num = new Random().Next(100, 1000);
+int num1 = num / 100; // Или int result = (num / 100) * 10 + num % 10;
+int num2 = num % 10;
+int result = num1 * 10 + num2;
+Console.WriteLine(num);
+Console.WriteLine($"Трехзначное число без второй цифры: {result}");
+// Или:
+Console.WriteLine($"{num} -> {num/100}{num%10}");
