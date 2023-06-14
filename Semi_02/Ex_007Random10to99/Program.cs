744ef8dc3aafdc9ab6011632a3c1aa459b069d2f
schlemuzzle <schlemuzzle@mail.ru>
2023-06-14 14:49:21 +0300

    Add Ex_007Random10to99+gitignore

---
+++ Semi_02/Ex_007Random10to99/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.Clear();
+int num = new Random().Next(10, 100);
+int num1 = num / 10;
+int num2 = num % 10;
+int max = num1;
+if (num2 > max)
+    max = num2;
+Console.WriteLine($"Наибольшая цифра в числе {num} - {max}");
