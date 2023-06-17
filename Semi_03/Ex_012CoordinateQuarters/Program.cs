1310a525dfb83f9fd482f9f14fd67eaec4afac14
schlemuzzle <schlemuzzle@mail.ru>
2023-06-17 12:42:21 +0300

    Add Ex_012CoordinateQuarters

---
+++ Semi_03/Ex_012CoordinateQuarters/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.Clear();
+Console.Write("Введите координату X: ");
+int x = int.Parse(Console.ReadLine());
+Console.Write("Введите координату Y: ");
+int y = int.Parse(Console.ReadLine());
+if (x > 0 && y > 0){
+    Console.WriteLine("1");
+}else if (x < 0 && y > 0){
+    Console.WriteLine("2");
+}else if (x < 0 && y < 0){
+    Console.WriteLine("3");
+}else if (x > 0 && y < 0){
+    Console.WriteLine("4");
+}else if (x == 0 && y == 0){
+    Console.WriteLine("На оси");
+}
