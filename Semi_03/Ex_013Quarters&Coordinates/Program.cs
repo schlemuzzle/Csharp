4c2f1bb0ec68a1b1d3c9cc04363acc6f9b346ec8
schlemuzzle <schlemuzzle@mail.ru>
2023-06-17 13:16:42 +0300

    Add Ex_013Quarters&Coordinates

---
+++ Semi_03/Ex_013Quarters&Coordinates/Program.cs
@@ -0,0 +1,22 @@
+﻿Console.Clear();
+Console.WriteLine("Введите номер четверти: ");
+int quart = int.Parse(Console.ReadLine());
+
+switch (quart)
+{
+    case 1:{
+        Console.WriteLine("Возможный диапазон координат: X>0 и Y>0");
+        break;}
+    case 2:{
+        Console.WriteLine("Возможный диапазон координат: X<0 и Y>0");
+        break;}
+    case 3:{
+        Console.WriteLine("Возможный диапазон координат: X<0 и Y<0");
+        break;}
+    case 4:{
+        Console.WriteLine("Возможный диапазон координат: X>0 и Y<0");
+        break;}
+    default:{ // default - если не одно из case не выполнилось - работает он.
+        Console.WriteLine("Введен неверный номер четверти");
+        break;}
+}
