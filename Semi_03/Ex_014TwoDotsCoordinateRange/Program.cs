b35329a7e67f5b2d97d8d6d059154c9c69e8fa92
schlemuzzle <schlemuzzle@mail.ru>
2023-06-17 13:58:08 +0300

    Add Ex_014TwoDotsCoordinateRange

---
+++ Semi_03/Ex_014TwoDotsCoordinateRange/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Clear();
+Console.Write("Введите первую координату A: ");
+double xA = double.Parse(Console.ReadLine());
+Console.Write("Введите вторую координату A: ");
+double yA = double.Parse(Console.ReadLine());
+Console.Write("Введите первую координату B: ");
+double xB = double.Parse(Console.ReadLine());
+Console.Write("Введите вторую координату B: ");
+double yB = double.Parse(Console.ReadLine());
+double result = Math.Sqrt(Math.Pow(xA - xB, 2) + Math.Pow(yA - yB, 2));
+Console.WriteLine($"Искомое расстояние: {result:f4}");// {X:fZ - Z знаков после запятой у Х}
