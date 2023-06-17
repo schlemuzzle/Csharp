dc813b734c71c120b079e6ddd2dbbfa35b9b3a30
schlemuzzle <schlemuzzle@mail.ru>
2023-06-17 14:43:53 +0300

    Add Ex_015_Squares1toN

---
+++ Semi_03/Ex_015_Squares1toN/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Clear();
+Console.Write("Введите число N: ");
+int n = int.Parse(Console.ReadLine());
+int i = 1;
+// for (i = 1; i <= n; i++); - чет не работает...
+if (n < i){
+    Console.Write("Ведено неверное число");
+    return;
+}
+Console.Write("Таблица квадратов числа N:");
+while (i <= n){
+    Console.Write($" {Math.Pow(i, 2)}");
+    i++;
+}
