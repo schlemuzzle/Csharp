a0e9924a29f9a84625824c2319ed821e24823e6d
schlemuzzle <schlemuzzle@mail.ru>
2023-06-17 15:09:45 +0300

    Add Ex_016_Squares1toN_Array+for

---
+++ Semi_03/Ex_016_Squares1toN_Array+for/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Clear();
+Console.Write("Введите число N: ");
+int N = int.Parse(Console.ReadLine());
+int[] result = new int [N];
+for (int i = 1; i <= N; i++)
+    result[i - 1] = i * i;
+//i-1 для расчета с нулевого индекса массива и до последнего (иначе считает с первого до последнего + 1)
+for (int i = 0; i < N; i++)
+    Console.Write($" {result[i]}");
+Console.WriteLine();
+Console.WriteLine("Второй способ: ");// без массива
+for (int i = 1; i <=N; i++)
+    Console.Write($" {i * i} ");
