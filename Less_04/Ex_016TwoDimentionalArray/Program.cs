7f27d231596fc519f3f2b65cf4d9453a3e205087
schlemuzzle <schlemuzzle@mail.ru>
2023-07-02 14:31:53 +0300

    Add lesson 04

---
+++ Less_04/Ex_016TwoDimentionalArray/Program.cs
@@ -0,0 +1,32 @@
+﻿Console.Clear();
+
+void PrintArray(int[,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            Console.Write($"{matr[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+void FillArray(int[,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            matr[i, j] = new Random().Next(1, 10);//[1; 10)
+        }
+    }
+}
+//                       matr.GetLength(0) - обращение к количеству строк
+//int[,] matrix = new int[3, 4];
+//                            matr.GetLength(1) - обращение к количеству столбцов
+
+int[,] matrix = new int[3, 4];
+
+FillArray(matrix);
+PrintArray(matrix);
---
+++ Less_04/Ex_018Factorial+Fibonacci/Program.cs
@@ -0,0 +1,21 @@
+﻿double Factorial(int n)
+{
+    if (n == 1) return 1;
+    else return n * Factorial(n - 1);
+}
+for (int i = 1; i < 10; i++)
+{
+    Console.WriteLine($"{i}! = {Factorial(i)}");
+}
+
+Console.WriteLine();
+
+double Fibonacci(int n)
+{
+ if(n == 1 || n == 2) return 1;
+ else return Fibonacci(n-1) + Fibonacci(n-2);
+}
+for (int i = 1; i < 10; i++)
+{
+ Console.WriteLine($"f({i}) = {Fibonacci(i)}");
+}
