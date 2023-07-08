e3a698c6383fde573b7f6f851dcc0bfcd96a15df
schlemuzzle <schlemuzzle@mail.ru>
2023-07-08 15:28:47 +0300

    Add Ex_022

---
+++ Less_05/Ex_022HanoyTower/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Clear();
+
+void Towers(string with = "1", string on = "3", string some = "2", int count = 4)
+//string with = "1" - начальный шпиль
+//string some = "2" - промежуточный шпиль
+//string on = "3"   - конечный шпиль
+//int count = 3     - кол-во блинчиков
+{
+    if (count > 1) Towers(with, some, on, count - 1);
+    Console.WriteLine($"{with} >> {on}");
+    if (count > 1) Towers(some, on, with, count - 1);
+}
+
+Towers();
