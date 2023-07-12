9c033ff0518fcbd2c62d6b4764e4dd47ef92cd5d
schlemuzzle <schlemuzzle@mail.ru>
2023-07-12 11:41:23 +0300

    Add Ex_01-03

---
+++ DZ_Semi_09/Ex_01 RecursionNto1/Program.cs
@@ -0,0 +1,21 @@
+﻿//Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от N до 1. Выполнить с помощью рекурсии.
+
+using System;
+using static System.Console;
+
+Clear();
+
+Write("Введите N: ");
+int n = int.Parse(ReadLine()!);
+
+WriteLine($"Ряд чисел от {n} до 1: [{PrintNumbers(n)}]");
+
+string PrintNumbers(int n)
+{
+    if (n == 1)
+    {
+        return "1";
+    }
+    string s = n.ToString() + ", " +  PrintNumbers(n - 1);
+    return s;
+}
---
+++ DZ_Semi_09/Ex_02RecursionSumMtoN/Program.cs
@@ -0,0 +1,20 @@
+﻿//Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+
+using System;
+using static System.Console;
+
+Clear();
+
+Write("Введите M: ");
+int m = int.Parse(ReadLine()!);
+Write("Введите N: ");
+int n = int.Parse(ReadLine()!);
+
+WriteLine($"Сумма цифр в промежутке от {m} до {n} = {SumDigits(m, n)}");
+
+int SumDigits(int m, int n)
+{
+    if (m == n)
+        return m;
+    return m + SumDigits(m + 1, n);
+}
